c645b35f58478edeb983c9ef69b15442f4d29a73
Clayton <ccatanzarite@bellarmine.edu>
2020-01-15 10:23:29 -0500

    Added Dog.cs and TestDog.cs

---
+++ Dog_CDC/Dog_CDC/Dog.cs
@@ -0,0 +1,70 @@
+﻿using System;
+/**
+ * This class models a Dog
+ * @author Clayton Catanzarite
+ * @version 1.0
+ */
+namespace Dog_CDC
+{
+    public class Dog
+    {
+        private int age;
+        private String name;
+        
+        public Dog()
+        {
+            age = 0;
+            name = "";
+        }
+        public Dog(int age, String name)
+        {
+            this.setAge(age);
+            this.setName(name);
+        }
+        public int calcDogYears()
+        {
+            int dogYears = 15 + (this.age - 1) * 7;
+            return dogYears;
+        }
+
+        public int getAge()
+        {
+            return age;
+        }
+
+        public void setAge(int age)
+        {
+            if (age < 0)
+                this.age = 0;
+            else
+                this.age = age;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public void setName(String name)
+        {
+            bool valid = true;
+            char[] n = name.ToCharArray();
+
+            foreach(char c in n){//foreach loop
+                if (!char.IsLetter(c)){//if statement
+                    valid = false;
+                    break;
+                }//if statement
+            }//foreach loop
+
+            if (valid)
+                this.name = name;
+            else
+                this.name = "";
+        }//setName
+        public String toString()
+        {
+            return "Dog [age=" + age + ", name=" + name + "]";
+        }
+    }
+}
---
+++ Dog_CDC/Dog_CDC/TestDog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dog_CDC
+{
+    public class TestDog
+    {
+        public static void Main(String[] args)
+        {
+            Dog firstDog = new Dog();
+
+            Console.WriteLine("What is the first dogs age?");
+            firstDog.setAge(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("What is the first dogs name?");
+            firstDog.setName(Console.ReadLine());
+            //set info for Dogs
+
+            Console.WriteLine(firstDog.toString());
+            Console.WriteLine(firstDog.getName() + "'s age in dog years is " + firstDog.calcDogYears());
+        }
+       
+    }
+}
